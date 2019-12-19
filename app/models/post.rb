8cdedaf62a7eddd41d1723f87587713819be9cd0
Vashira <vashimgs@gmail.com>
2019-12-19 17:04:34 +0000

    Added maximum length for the body

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { maximum: 50 }
-  validates :body, presence: true
+  validates :body, presence: true, length: {maximum: 150}
 end
