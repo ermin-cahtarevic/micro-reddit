aae5c6547cd666f99cdf4fc5c66166bfb05dc051
Ermin Cahtarevic <erminc@windowslive.com>
2019-12-19 18:38:52 +0100

    Added the Comment model and the validations needed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@
 
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true, length: { maximum: 150 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
   validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
 end
